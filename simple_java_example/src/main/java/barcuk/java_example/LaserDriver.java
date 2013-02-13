c11a31a16cc6b124f8b5ace2e921e40a2969cb88
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2013-02-13 13:30:09 +0000

    added example package

---
+++ simple_java_example/src/main/java/barcuk/java_example/LaserDriver.java
@@ -0,0 +1,66 @@
+package barcuk.java_example;
+
+import geometry_msgs.Twist;
+import geometry_msgs.Vector3;
+
+import org.apache.commons.logging.Log;
+import org.ros.message.MessageListener;
+import org.ros.namespace.GraphName;
+import org.ros.node.AbstractNodeMain;
+import org.ros.node.ConnectedNode;
+import org.ros.node.topic.Publisher;
+import org.ros.node.topic.Subscriber;
+
+import sensor_msgs.LaserScan;
+
+public class LaserDriver extends AbstractNodeMain {
+
+	@Override
+	public GraphName getDefaultNodeName() {
+		return GraphName.of("laser_driver");
+	}
+
+	@Override
+	public void onStart(ConnectedNode _node) {
+
+		// Use a logger instead of the usual System.out.println to get send
+		// output to ROS.
+		//
+		// This is final so we can use it in the anonymous inner class below
+		final Log logger = _node.getLog();
+
+		// Create an object used to publish Twist messages to the cmd_vel topic
+		// in order to control the robot
+		final Publisher<Twist> publisher = _node.newPublisher("cmd_vel", Twist._TYPE);
+
+		// Subscribe to laser scans published on the base_scan topic
+		Subscriber<LaserScan> subscriber = _node.newSubscriber("base_scan",
+				LaserScan._TYPE);
+
+		// Add a listener that is called each time a new laser scan is received.
+		subscriber.addMessageListener(new MessageListener<LaserScan>() {
+			@Override
+			public void onNewMessage(LaserScan _scan) {
+				float[] ranges = _scan.getRanges();
+				float maxRange = Float.MIN_VALUE;
+				for (int i = 0; i < ranges.length; i++) {
+					if (ranges[i] > maxRange) {
+						maxRange = ranges[i];
+					}
+				}
+				logger.info("The longest range I found was: " + maxRange);
+				logger.info("I hope this is less than: " + _scan.getRangeMax());
+			
+				//create a new Twist message
+				Twist twist = publisher.newMessage();
+				//set the desired forward velocity of the robot to 0.2 m/s.
+				Vector3 linear = twist.getLinear();
+				linear.setX(0.2);
+				twist.setLinear(linear);
+				//publish method to robot
+				publisher.publish(twist);
+			}
+
+		});
+	}
+}
